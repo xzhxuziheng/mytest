c8f35efc7c3620438d2d9b2eddde28290b8cdd4d
xzhxuziheng <463235820@qq.com>
2022-05-23 18:34:19 +0800

    '接口依赖test1'

---
+++ t.py
@@ -0,0 +1,3 @@
+li = ['0', 'data', 'list', '0', 'orderNo']
+for i in li:
+    print(i, type(i))
--- web_testcase/api/test_all_api.py
+++ web_testcase/api/test_all_api.py
@@ -4,27 +4,23 @@ from common.requests_util import RequestsUtil
 from common.common_util import CommonUtil
 
 
-class TestOrderPlatformRide:
+class TestAllApi:
 
     base_url = CommonUtil.base_url
     header = CommonUtil.header
     response_data = []
 
-    # 获取骑行订单列表
-    @pytest.mark.parametrize('caseinfo', yaml_util.read_yaml('/web_testcase/case/yaml_data/order/rideOrderList.yaml'))
+    @pytest.mark.parametrize('caseinfo', yaml_util.read_yaml('/web_testcase/case/all_api.yaml'))
     def test_get_order_platform_ride_list(self, caseinfo):
         name = caseinfo['name']
-        url = TestOrderPlatformRide.base_url+caseinfo['url']
+        url = TestAllApi.base_url+caseinfo['url']
         method = caseinfo['method']
-        header = TestOrderPlatformRide.header
+        header = TestAllApi.header
         token = {'token': yaml_util.read_yaml('/web_testcase/case/dependCase.yaml')['token']}
-        print(token)
         header.update(token)
-        print(header)
         data = caseinfo['data']
-        print('测试接口：%s' % name)
         res = RequestsUtil.session.request(method=method, url=url, headers=header, params=data)
-        TestOrderPlatformRide.response_data.append(res.json())
+        TestAllApi.response_data.append(res.json())
 
     def get_response_data(self, response_data):
         length = len(response_data)
